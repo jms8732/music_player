da4c5957f59c02d957d0487beb8207910a2bd4cc
jang Min suk <jms8732@naver.com>
2021-01-03 21:55:00 +0900

    2021-01-03

---
+++ app/src/main/java/com/example/myapplication/ItemMoveCallback.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemMoveCallback extends ItemTouchHelper.Callback {
+    private final ItemTouchHelperAdapter adapter;
+    public ItemMoveCallback(ItemTouchHelperAdapter adapter) {
+        this.adapter = adapter;
+    }
+
+    @Override
+    public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        int flagDrag = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        int flagSwipe = ItemTouchHelper.START | ItemTouchHelper.END;
+
+        return makeMovementFlags(flagDrag,flagSwipe);
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        adapter.onItemMove(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+        adapter.onItemDismiss(viewHolder.getAdapterPosition());
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isItemViewSwipeEnabled() {
+        return true;
+    }
+
+    public interface ItemTouchHelperAdapter{
+        void onItemMove(int fromPos, int targetPos);
+        void onItemDismiss(int pos);
+    }
+}
